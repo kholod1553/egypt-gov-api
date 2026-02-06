606a156446d912f464754650952f98a4733567e1
khokha1553 <khokha1553@gmail.com>
2026-02-07 01:47:51 +0200

    Final version with correct env keys and vercel

--- egypt-gov-api/src/app.js
+++ egypt-gov-api/src/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.set('json spaces', 2);
 app.use(cors());
 app.use(express.json());
-app.use('/api', servicesRoutes);
+app.use('/api/services', servicesRoutes);
 app.get("/", (_req, res) => {
   res.send("Egypt Gov API is running...");
 });
