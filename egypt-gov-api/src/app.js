9389c293952ec6dbe112746cc8af9c47821ac0b6
khokha1553 <khokha1553@gmail.com>
2026-02-07 02:44:41 +0200

    Correct vercel rewrites to src/app.js

--- egypt-gov-api/src/app.js
+++ egypt-gov-api/src/app.js
@@ -5,8 +5,9 @@ const app = express();
 app.set('json spaces', 2);
 app.use(cors());
 app.use(express.json());
+app.get('/favicon.ico', (req, res) => res.status(204).end());
 app.use('/api/services', servicesRoutes);
-app.get("/", (_req, res) => {
+app.get("/", (req, res) => {
   res.send("Egypt Gov API is running...");
 });
 export default app;
