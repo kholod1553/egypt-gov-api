c87431da86d82b69e1e5f4af5630f492db332f28
khokha1553 <khokha1553@gmail.com>
2026-02-08 10:06:21 +0200

    fix paths

---
+++ egypt-gov-api/src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import servicesRoutes from "./routes/services.routes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/services", servicesRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Egypt Gov API is running...");
+});
+
+export default app;
