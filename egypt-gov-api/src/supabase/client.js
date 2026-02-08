19883165ab0f455ccee69bbe332432c03545d0d0
khokha1553 <khokha1553@gmail.com>
2026-02-08 10:51:10 +0200

    url

--- egypt-gov-api/src/supabase/client.js
+++ egypt-gov-api/src/supabase/client.js
@@ -5,5 +5,7 @@ dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+console.log("Supabase URL:", process.env.SUPABASE_URL);
+console.log("Supabase Key:", process.env.SUPABASE_SERVICE_ROLE_KEY);
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
