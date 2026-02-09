2a274f0453190024f30b9c4d30c2ded33de262ae
khokha1553 <khokha1553@gmail.com>
2026-02-09 04:45:11 +0200

    simple edit

--- egypt-gov-api/src/controllers/services.controller.js
+++ egypt-gov-api/src/controllers/services.controller.js
@@ -7,12 +7,15 @@ export const getAllServices = async (req, res) => {
             .select('*');
 
         if (error) throw error;
-res.status(200).send(JSON.stringify({
-    success: true,
-    results: data.length,
-    data: data
-}, null, 2));
+
+        // التعديل هنا: استخدمي .json() مباشرة ومرري الـ Object
+        res.status(200).json({
+            success: true,
+            results: data.length,
+            data: data
+        });
+
     } catch (error) {
-        res.status(500).json({ success: false, error: error.message});
+        res.status(500).json({ success: false, error: error.message });
     }
 };
